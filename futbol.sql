da831879e8aac647a7231878f75253368d2da438
GiselaDiazGomez <30881032+GiselaDiazGomez@users.noreply.github.com>
2017-09-18 13:40:01 -0500

    Add files via upload

---
+++ futbol.sql
@@ -0,0 +1,15 @@
+use futbolprofesional;
+insert equipo (idequipo, ciudad, nombre, iddepartamento, color, estrellas, estrellascompradas ) 
+values        (1,'Bogota','Inependiente SantaFe',0, 'Rojo y blanco',9,0),
+              (2,'Medellin','Atletico Nacional',0,'Verde y blanco',16,0),
+              (3,'Barranquilla','Atletico Junior',0,'Azul, verde y blanco',7,0);
+insert into jugador (idjugador, nombre, estatura, edad, posicion) values 
+					(1, 'RÓBINSON ZAPATA')
+				    (2, 'MIGUEL SOLIS')
+                    (3, 'LEANDRO CASTELLANOS')
+insert into panel (idequipo, idjugador) values () 
+
+
+
+
+			
